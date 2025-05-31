16c4d61c7f084ff5f0c27350bb07e03d835bddb9
Yousef Sayed <uchiha.joee@gmail.com>
2025-05-31 14:55:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ CORS(app)  # تمكين CORS للجميع
 # تهيئة SocketIO مع إصدارات متوافقة
 socketio = SocketIO(app,
                   cors_allowed_origins="*",
-                  async_mode='eventlet',
+                  async_mode='threading',
                   engineio_logger=True,
                   logger=True,
                   ping_timeout=60,
